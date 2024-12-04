61565a3ec8d8b9455dce00360be4290850ead1be
ErwiniaDev <istar_arwene@hotmail.fr>
2024-12-04 17:25:32 +0100

    #4 - Ended project

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function verticalSlide() {
-    let slideValue = document.getElementById("vertical-slider").value;
+    let slideValue = 100 - document.getElementById("vertical-slider").value;
 
-    document.getElementById("my-vertical-img").style.clipPath="polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)"
+    document.getElementById("my-vertical-img").style.clipPath="inset(0 " + slideValue + "% 0 0)"
 }
 
 function horizontalSlide() {
-    let slideValue = 100 - (document.getElementById("horizontal-slider").value);
+    let slideValue = 100 - document.getElementById("horizontal-slider").value;
 
-    document.getElementById("my-horizontal-img").style.clipPath="polygon(0 " + slideValue + "%, 100% " + slideValue + "%, 100% 100%, 0 100%)"
+    document.getElementById("my-horizontal-img").style.clipPath="inset(" + slideValue + "% 0 0 0)"
 }
 
 function redirect(url) {
